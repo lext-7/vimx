9694845d5d639bab7f05d60c8b42016ae8c9c611
lext <lext.nobody@outlook.com>
2016-06-29 16:31:17 +0800

    check node

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir ~/.vim/bundle -p
 
-sudo apt-get install ctags
-sudo apt-get install build-essential cmake python-dev  #编译YCM自动补全插件依赖
-sudo apt-get install silversearcher-ag
+sudo apt install ctags build-essential cmake python-dev silversearcher-ag
 
 BASEDIR=$(dirname $0)
 cp $BASEDIR/.vimrc  ~/.vimrc
@@ -12,4 +10,8 @@ cp $BASEDIR/.vimrc.bundles  ~/.vimrc.bundles
 vim -u $HOME/.vimrc.bundles +PlugInstall! +PlugClean! +qall
 cd $HOME/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
-./install.sh --tern-completer --clang-completer
+if [ -z $(which node) ]; then
+    ./install.sh --clang-completer
+else
+    ./install.sh --tern-completer --clang-completer
+fi
