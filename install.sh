98c2f460189f0d5fb60c5d61ace8e3e5c61ce825
lext <lext.nobody@outlook.com>
2016-06-29 13:53:49 +0800

    change the way to build YouCompleteMe

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir ~/.vim/bundle -p
+
+sudo apt-get install ctags
+sudo apt-get install build-essential cmake python-dev  #编译YCM自动补全插件依赖
+sudo apt-get install silversearcher-ag
+
 BASEDIR=$(dirname $0)
 cp $BASEDIR/.vimrc  ~/.vimrc
 cp $BASEDIR/.vimrc.bundles  ~/.vimrc.bundles
 vim -u $HOME/.vimrc.bundles +PlugInstall! +PlugClean! +qall
-$HOME/.vim/bundle/YouCompleteMe/install.sh --all
-# cp $BASEDIR/.vimrc  ~/.vimrc
-# cp $BASEDIR/.vimrc.bundles  ~/.vimrc.bundles
+cd $HOME/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.sh --tern-completer --clang-completer
