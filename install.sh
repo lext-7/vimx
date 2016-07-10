05b5a108729a3af1a8351c0e35943f23a1b24844
lext <lext.nobody@outlook.com>
2016-07-10 15:04:46 +0800

    support install without youcompletme

--- install.sh
+++ install.sh
@@ -2,16 +2,20 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir ~/.vim/bundle -p
 
-sudo apt install ctags build-essential cmake python-dev silversearcher-ag
+if ! [[ "$1" == "--simple-mode" ]]; then
+    sudo apt install ctags build-essential cmake python-dev silversearcher-ag llvm
+fi
 
 BASEDIR=$(dirname $0)
 cp $BASEDIR/.vimrc  ~/.vimrc
 cp $BASEDIR/.vimrc.bundles  ~/.vimrc.bundles
 vim -u $HOME/.vimrc.bundles +PlugInstall! +PlugClean! +qall
 cd $HOME/.vim/bundle/YouCompleteMe
-git submodule update --init --recursive
-if [ -z $(which node) ]; then
-    ./install.sh --clang-completer
-else
-    ./install.sh --tern-completer --clang-completer
+
+if ! [[ "$1" == "--simple-mode" ]]; then
+    if [ -z $(which node) ]; then
+        ./install.sh --clang-completer --system-libclang
+    else
+        ./install.sh --tern-completer --clang-completer --system-libclang
+    fi
 fi
